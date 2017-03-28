be008fcda3a21057233317f984504e1dff2db55c
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-03-27 21:19:08 -0700

    connect components to code

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Mylabel: UILabel!
+    @IBAction func getOmikuji(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
